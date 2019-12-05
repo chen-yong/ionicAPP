f2e62ec9365ca0ee4582c36a0d60b7f40cebdd84
cy陈勇 <1208820585@qq.com>
2019-12-05 14:28:45 +0800

    引入ng-zooro-mobile

--- tabs/src/main.ts
+++ tabs/src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'hammerjs'; // 引入ionic4 中的gestures 手势事件包括: tap, press, pan, swipe, rotate, and pinch events 等。
+
 
 if (environment.production) {
   enableProdMode();
